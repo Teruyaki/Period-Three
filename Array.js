948f2a406f05291a6cb42a6f0ad289f9c514781b
Teruyaki <30877943+Teruyaki@users.noreply.github.com>
2017-08-16 10:21:03 -1000

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,4 @@
+var juniors = ["Reimon", "Hana"];
+var seniors = ["samiyah", "Justin", "bryson"];
+var students =
+window.alert(" There are" + " " + juniors.length + " juniors" + " " + seniors.length + " seniors");
